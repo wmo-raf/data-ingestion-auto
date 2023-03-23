67efad9fe917b13e2dc4978fc67f326c581023a2
Erick Otenyo <otenyo.erick@gmail.com>
2023-03-23 16:59:46 +0300

    uncomment jobs

--- ingest/jobs.py
+++ ingest/jobs.py
@@ -21,27 +21,27 @@ chirps_rainfall_estimate = ChirpsRainfall(dataset_id="chirps_rainfall",
 
 # Jobs
 jobs = [
-    # {
-    #     "job": dust_forecast.task,
-    #     "options": {
-    #         'trigger': "interval", "seconds": int(SETTINGS.get("DUST_FORECAST_UPDATE_INTERVAL_SECONDS")),
-    #         "max_instances": 1
-    #     }
-    # },
-    # {
-    #     "job": ecmwf_forecast.task,
-    #     "options": {
-    #         'trigger': "interval", "seconds": int(SETTINGS.get("ECMWF_FORECAST_UPDATE_INTERVAL_SECONDS")),
-    #         "max_instances": 1
-    #     }
-    # },
-    # {
-    #     "job": tamsat_rainfall_estimate.task,
-    #     "options": {
-    #         'trigger': "interval", "seconds": int(SETTINGS.get("TAMSAT_RAINFALL_UPDATE_INTERVAL_SECONDS")),
-    #         "max_instances": 1
-    #     }
-    # },
+    {
+        "job": dust_forecast.task,
+        "options": {
+            'trigger': "interval", "seconds": int(SETTINGS.get("DUST_FORECAST_UPDATE_INTERVAL_SECONDS")),
+            "max_instances": 1
+        }
+    },
+    {
+        "job": ecmwf_forecast.task,
+        "options": {
+            'trigger': "interval", "seconds": int(SETTINGS.get("ECMWF_FORECAST_UPDATE_INTERVAL_SECONDS")),
+            "max_instances": 1
+        }
+    },
+    {
+        "job": tamsat_rainfall_estimate.task,
+        "options": {
+            'trigger': "interval", "seconds": int(SETTINGS.get("TAMSAT_RAINFALL_UPDATE_INTERVAL_SECONDS")),
+            "max_instances": 1
+        }
+    },
     {
         "job": chirps_rainfall_estimate.task,
         "options": {
